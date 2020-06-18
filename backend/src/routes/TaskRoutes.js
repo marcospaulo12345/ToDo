8e5c2345bd3f4d5df8c267f3a235fce2ef405a59
Marcos Paulo <marcospaulo.mpf34@gmail.com>
2020-06-18 20:36:32 -0300

    Finalizado o BackEnd, com a API concluida com todas as rotas para nossa aplicação tanto mobile quanto WEB

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -3,11 +3,19 @@ const router = express.Router();
 
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
-const MacaddressValidation = require('../middlewares/MacaddressValidation');
+const taskModel = require('../model/taskModel');
 
 router.post('/', TaskValidation,TaskController.create);
 router.put('/:id', TaskController.update);
+router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
+router.put('/:id/:done', TaskController.done);
 
-router.get('/filter/all', MacaddressValidation, TaskController.all);
+router.get('/filter/all/:macaddress', TaskController.all);
+router.get('/filter/late/:macaddress', TaskController.late);
+router.get('/filter/today/:macaddress', TaskController.today);
+router.get('/filter/week/:macaddress', TaskController.week);
+router.get('/filter/month/:macaddress', TaskController.month);
+router.get('/filter/year/:macaddress',TaskController.year);
 
 module.exports = router;
